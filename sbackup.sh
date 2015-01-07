a65ab6e2791fb972d4ea0778c9fdee942c8621d6
Nebojša Jakovljević <nebojsa@nebjak.net>
2015-01-07 16:56:05 +0100

    Initial commit

---
+++ sbackup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ -e $1 && -e $1 ]]
+then 
+  backup_src=$1
+else
+  echo "Source for backup is not specified"
+  exit 1
+fi
+
+if [ $2 ]
+then
+  backup_name=$2
+else
+  backup_name=$(basename $1)
+fi
+
+backup_dir="~/$(date +%Y%m%d-%H%M%S)"
+
+mkdir -p $backup_dir
+echo "Creating backup direcory at: $backup_dir"
+
+tar cJf "$backup_dir/$backup_name.tar.xz" "$backup_src"
+
